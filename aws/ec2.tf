fe6acab0f2ec98603c7b42b03188a26ffb22f64b
milindrai <milindrai365@gmail.com>
2024-08-29 15:37:06 +0530

    initial commit

---
+++ aws/ec2.tf
@@ -0,0 +1,9 @@
+# AWS EC2 Instance
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"  # Replace with a valid AMI ID for your region
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "example-instance"
+  }
+}
